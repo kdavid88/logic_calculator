0eac7e6e043caa2b58dbcf28440fb0ace24a625c
David Kertesz <kdavid88@gmail.com>
2023-05-20 14:20:06 +0200

    Adding variables, control for subformulas, buttons for operations.

--- src/main/java/controller/LogicCalculatorController.java
+++ src/main/java/controller/LogicCalculatorController.java
@@ -1,4 +1,42 @@
 package controller;
 
+import javafx.beans.Observable;
+import javafx.beans.value.ObservableStringValue;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+
 public class LogicCalculatorController {
+     @FXML
+     private ListView formulaList;
+
+     @FXML
+     private Label leftFormulaLabel, righFormulaLabel;
+     @FXML
+     private TextField newVariable;
+
+
+
+
+     @FXML
+     public void initialize(){
+
+     }
+
+     public void formulaToLEft(){
+          String leftFormula = (String) formulaList.getSelectionModel().getSelectedItem();
+          leftFormulaLabel.setText(leftFormula);
+     }
+     public void formulaToRight(){
+          String rightFormula = (String) formulaList.getSelectionModel().getSelectedItem();
+          righFormulaLabel.setText(rightFormula);
+     }
+
+     public void addNewVariable(){
+          String newVariableName = newVariable.getText();
+          formulaList.getItems().add(newVariableName);
+          newVariable.clear();
+     }
 }
